39b77e2b9a5244aa0b1571c6e08020d67a87dd13
Yingjie <yingjie.guan@thoughtworks.com>
2020-12-28 22:46:04 +0800

    [yingjie]finish guess number and start modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+//queryS取值，赋值给变量，
+
+const showmodal = document.querySelectorAll('.show-modal');
+const hiddenBtn = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+console.log(showmodal);
+
+for (let i = 0; i < showmodal.length; i++) {
+  console.log(showmodal[i].textContent);
+}
