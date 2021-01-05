a6a18220fc5270f7e56774359b66b6e9f006187a
Yingjie <yingjie.guan@thoughtworks.com>
2021-01-05 22:03:48 +0800

    [yingjie]add model demo

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,11 +1,29 @@
 'use strict';
 //queryS取值，赋值给变量，
-
-const showmodal = document.querySelectorAll('.show-modal');
+//给showbutton添加一个点击监听事件，change the class name ,use classList.remove
+//add keyboard events
+const showmodalBtn = document.querySelectorAll('.show-modal');
 const hiddenBtn = document.querySelector('.close-modal');
 const overlay = document.querySelector('.overlay');
-console.log(showmodal);
-
-for (let i = 0; i < showmodal.length; i++) {
-  console.log(showmodal[i].textContent);
+const modal = document.querySelector('.modal');
+// console.log(showmodal);
+const openModel = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+const closeModel = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+for (let i = 0; i < showmodalBtn.length; i++) {
+  console.log(showmodalBtn[i].textContent);
+  showmodalBtn[i].addEventListener('click', openModel);
 }
+hiddenBtn.addEventListener('click', closeModel);
+//添加键盘事件
+document.addEventListener('keydown', function (e) {
+  console.log(e.key);
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModel();
+  }
+});
